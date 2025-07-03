485835e00937fde773768eeb49b31d9a6bf9bddc
ahmed <ahmed.youssef.5522.52@gmail.com>
2025-07-03 11:21:44 +0300

    feat(landing page): Hero section completed

--- src/layout/Router.jsx
+++ src/layout/Router.jsx
@@ -3,11 +3,13 @@ import RootLayout from "./RootLayout";
 import { Route } from "react-router-dom"
 import { useState } from "react";
 import SignInPage from "@/Pages/SignInPage";
+import Home from "@/Pages/Landing page/Home";
 
 export const Router = createBrowserRouter(
     createRoutesFromChildren(
         <Route path="/" element={<RootLayout />}>
-            <Route path="/login" element={<SignInPage/>}/>
+            <Route index element={<Home />} />
+            <Route path="/login" element={<SignInPage />} />
             <Route path="*" element={<h1>404</h1>} />
         </Route>
     )
